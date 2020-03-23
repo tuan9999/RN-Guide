19743351348d7689bab2824914acbc5317118314
tuan perera <tuanperera@gmail.com>
2020-03-23 11:49:33 +0100

    u

---
+++ App.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+import {
+	StyleSheet,
+	Text,
+	View,
+	ScrollView,
+	TextInput,
+	Button,
+	FlatList } from 'react-native';
+
+import GoalItem from './components/GoalItem';
+import GoalInput from './components/GoalInput';
+
+export default function App() {
+	const [courseGoals, setCourseGoals] = useState([]);
+
+	const addGoalHandler = goalTitle => {
+		setCourseGoals(courseGoals => [
+			...courseGoals,
+			{ id: Math.random().toString(), value: goalTitle }
+		]);
+	};
+
+	const removeGoalHandler = goalId => {
+		setCourseGoals(currentGoals => {
+			return currentGoals.filter((goal) => goal.id !== goalId);
+		});
+	}
+
+  return (
+    <View style={ styles.screen }>
+			<GoalInput onAddGoal={addGoalHandler} />
+			<FlatList
+				keyExtractor={(item, index) => item.id}
+				data={courseGoals}			
+				renderItem={itemData => (<GoalItem id={itemData.item.id} onDelete={removeGoalHandler} title={itemData.item.value} />)}
+			/>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+	screen: {
+		padding: 50,
+	},
+});
